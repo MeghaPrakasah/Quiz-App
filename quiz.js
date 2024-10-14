7ee1a7f9f6395c2e2e1f8c8d46344452e8292b71
megha prakasah <meghatp12345@gmail.com>
2024-10-14 12:13:12 +0530

    something new

--- quiz.js
+++ quiz.js
@@ -9,6 +9,64 @@ const resultBox = document.querySelector('.result-box');
 const tryAgainBtn = document.querySelector('.tryAgain-btn');
 const goHomeBtn = document.querySelector('.goHome-btn');
 
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Codehal Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Hometext Programming",
+            "C. Hypertext Preprogramming",
+            "D. Programming Hypertext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "D. Structured Query Language",
+        options: [
+            "A. Strength Query Language",
+            "B. Stylesheet Query Language",
+            "C. Science Question Language",
+            "D. Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Language",
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language"
+        ]
+    }
+];
+
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
     main.classList.add('active');
@@ -23,5 +81,42 @@ continueBtn.onclick = () => {
     quizSection.classList.add('active');
     popupInfo.classList.remove('active');
     main.classList.remove('active');
+    quizBox.classList.add('active');
+
+    showQuestions(0)
+    questionCounter(1)
+}
+
+let questionCount = 0;
+let questionNumb=1;
+const nextBtn = document.querySelector('.next-btn');
+const optionList = document.querySelector('.option-list');
+
+nextBtn.onclick = () => {
+    if(questionCount < questions.length - 1 ){
+        questionCount++;
+        showQuestions(questionCount);
+
+        questionNumb++;
+        questionCounter(questionNumb)
+    }
+    else{
+        console.log('Question Completed');
+        
+    }
+    
+}
+//getting questions and options from array
+function showQuestions(index) {
+    const questionText = document.querySelector(".question-text");
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+                    <div class="option"><span>${questions[index].options[1]}</span></div>
+                    <div class="option"><span>${questions[index].options[2]}</span></div>
+                    <div class="option"><span>${questions[index].options[3]}</span></div>
+                    `;
+
+    optionList.innerHTML = optionTag;
 
 }
